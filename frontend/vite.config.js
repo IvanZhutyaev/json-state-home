a5141ce55ad9701c8056217b14f57f88ca1970c4
ModderCodder <1modder.path1@gmail.com>
2025-06-28 17:08:30 +0300

    webapp 2.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Наш.Дом - Поиск недвижимости',
+        short_name: 'Наш.Дом',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007aff',
+        description: 'Поиск новостроек и недвижимости в России',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
@@ -23,6 +43,6 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    allowedHosts: ['90cf-45-8-98-163.ngrok-free.app'],
+    allowedHosts: ['8a16-45-8-98-163.ngrok-free.app'],
   },
 })
